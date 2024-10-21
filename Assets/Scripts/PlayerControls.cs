0bd8040850de25ad231c7e260918bacd16400ce9
BeetleZeus <oelshark@gmail.com>
2024-10-21 15:47:14 -0700

    basic control mechanics added
    added ship controls , updated speed & range of motion to prevent ship from veering off screen.
    
    we now have a functional ship that flies on a rail, and is controlled by the player

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    private float horizontalMovement;
-    private float verticalMovement;
-    [SerializeField] InputAction movement;
-    [SerializeField] InputAction fire;
+    private float xMovement;
+    private float yMovement;
+    [SerializeField] int offsetBoost = 38; // variable for adjusting movement speed on player input
+    [SerializeField] float xRange = 12.0f;    // range of movement to prevent ship from going offscreen
+    [SerializeField] float yRange = 7.0f;    // range of movement to prevent ship from going offscreen
+
+    [SerializeField] InputAction movement; //input action for setting bindings for player movement
+    [SerializeField] InputAction fire;     // input action for setting bindings for player shooting
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,17 +33,28 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMovement = movement.ReadValue<Vector2>().x;
-        verticalMovement = movement.ReadValue<Vector2>().y;
+        xMovement = movement.ReadValue<Vector2>().x;
+        yMovement = movement.ReadValue<Vector2>().y;
+
+        float xOffset = xMovement * Time.deltaTime * offsetBoost;
+        float yOffset = yMovement * Time.deltaTime * offsetBoost;
+
+        float offsetXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(offsetXPos, -xRange, xRange);
+
+        float offsetYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(offsetYPos, -yRange, yRange);
 
         //float horizontalMovement = Input.GetAxis("Horizontal");
         //float verticalMovement = Input.GetAxis("Vertical");
 
-        if (horizontalMovement != 0 || verticalMovement != 0)
-        {
-            Debug.Log("horizontal movement" + horizontalMovement);
-            Debug.Log("vertical movement" + verticalMovement);
-        }
+        //if (xMovement != 0 || yMovement != 0)
+        //{
+        //    Debug.Log("horizontal movement" + xMovement);
+        //    Debug.Log("vertical movement" + yMovement);
+        //}
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
 
     }
 }
