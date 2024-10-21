0268cde2f116eed18fbd8aee4310da75400ab468
BeetleZeus <oelshark@gmail.com>
2024-10-20 19:29:42 -0700

    Using Unity Input System
    updated the player controls script to use both Unity Input Manager and Input System. Added bindings for all key movements.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    private float horizontalMovement;
+    private float verticalMovement;
+    [SerializeField] InputAction movement;
+    [SerializeField] InputAction fire;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        horizontalMovement = movement.ReadValue<Vector2>().x;
+        verticalMovement = movement.ReadValue<Vector2>().y;
+
+        //float horizontalMovement = Input.GetAxis("Horizontal");
+        //float verticalMovement = Input.GetAxis("Vertical");
 
         if (horizontalMovement != 0 || verticalMovement != 0)
         {
             Debug.Log("horizontal movement" + horizontalMovement);
             Debug.Log("vertical movement" + verticalMovement);
         }
-        
+
     }
 }
