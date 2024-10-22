d8f43ce05648f62dd51618484d86735ea9e49384
BeetleZeus <oelshark@gmail.com>
2024-10-21 19:34:41 -0700

    added Ship rotation
    this commit contains base logic for ship rotation while maneuvering horizontally & vertically

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    private float xMovement;
-    private float yMovement;
-    [SerializeField] int offsetBoost = 38; // variable for adjusting movement speed on player input
+    private float xMovementInput;
+    private float yMovementInput;
+    private float pitch;
+    private float yaw;
+    private float roll;
+    [SerializeField] int offsetBoost = 32; // variable for adjusting movement speed on player input
     [SerializeField] float xRange = 12.0f;    // range of movement to prevent ship from going offscreen
     [SerializeField] float yRange = 7.0f;    // range of movement to prevent ship from going offscreen
+
+    [SerializeField] float positionPitchCoefficient = -2.0f;
+    [SerializeField] float controlPitchCoefficient = -15.0f;
+
+    [SerializeField] float positionYawCoefficient = 2.0f;
+    [SerializeField] float controlRollCoefficient = -30.0f;
 
     [SerializeField] InputAction movement; //input action for setting bindings for player movement
     [SerializeField] InputAction fire;     // input action for setting bindings for player shooting
@@ -33,11 +42,18 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xMovement = movement.ReadValue<Vector2>().x;
-        yMovement = movement.ReadValue<Vector2>().y;
+        ShipPosition();
+        ShipRotation();
 
-        float xOffset = xMovement * Time.deltaTime * offsetBoost;
-        float yOffset = yMovement * Time.deltaTime * offsetBoost;
+    }
+
+    void ShipPosition()
+    {
+        xMovementInput = movement.ReadValue<Vector2>().x;
+        yMovementInput = movement.ReadValue<Vector2>().y;
+
+        float xOffset = xMovementInput * Time.deltaTime * offsetBoost;
+        float yOffset = yMovementInput * Time.deltaTime * offsetBoost;
 
         float offsetXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(offsetXPos, -xRange, xRange);
@@ -55,6 +71,18 @@ public class PlayerControls : MonoBehaviour
         //}
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
 
+    void ShipRotation()
+    {
+        float positionPitch = transform.localPosition.y * positionPitchCoefficient;
+        float controlPitch = yMovementInput * controlPitchCoefficient;
+        pitch = positionPitch + controlPitch;
+
+        yaw = transform.localPosition.x * positionYawCoefficient; ;
+
+        roll = xMovementInput * controlRollCoefficient;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
