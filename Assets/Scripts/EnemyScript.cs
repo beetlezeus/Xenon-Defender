0adcfff37dec29852415f09076c9de3dfdebc45b
BeetleZeus <oelshark@gmail.com>
2024-10-28 17:22:35 -0700

    Added enemy killpoints
    in addition to the enemy hit points, in this commit we added "enemy kill points" to act as bonus for killing the enemy.
    
    We also created some enemy prefabs using the Star Sparrow assets pack so we can have a lot of enemy variety in the game

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,20 +7,29 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] ParticleSystem enemyExplosionVFX;
     [SerializeField] ParticleSystem enemyHitVFX;
     [SerializeField] Transform tempParent;
-    [SerializeField] int enemyHealth = 1;
-    [SerializeField] int enemyScorePoints = 100;
+    [SerializeField] int enemyHealth = 1;      // how many hits to kill enemy
+    [SerializeField] int enemyHitPoints = 100; // how much to award for each enemy hit
+    [SerializeField] int enemyKillPoints = 500; // how much to award for a kill
 
     private PlayerScore playerScore;
+    private Rigidbody enemyRB;
 
     private void Start()
     {
         playerScore = GameObject.Find("Game Manager").GetComponent<PlayerScore>();
+        AddRB();
+    }
+
+    private void AddRB()
+    {
+        enemyRB = gameObject.AddComponent<Rigidbody>();
+        enemyRB.useGravity = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
         DecrementEnemyHealth(1);
-        UpdatScore();
+        UpdatScore(enemyHitPoints);
 
         if (enemyHealth <= 0)
         {
@@ -28,9 +37,9 @@ public class EnemyScript : MonoBehaviour
         }
     }
 
-    private void UpdatScore()
+    private void UpdatScore(int points)
     {
-        playerScore.UpdateEnemyKillScore(enemyScorePoints);
+        playerScore.UpdateEnemyKillScore(points);
     }
 
     public void DecrementEnemyHealth(int damageAmount)
@@ -42,6 +51,7 @@ public class EnemyScript : MonoBehaviour
 
     void StartDeathSequence()
     {
+        UpdatScore(enemyKillPoints);
         ParticleSystem deathVFX = Instantiate(enemyExplosionVFX, transform.position, Quaternion.identity);
         deathVFX.transform.parent = tempParent;
         Destroy(gameObject);
